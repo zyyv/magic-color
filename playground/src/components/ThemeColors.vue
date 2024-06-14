934c6f5679851022be07df5423f4f5ff18b55ced
Chris <hizyyv@gmail.com>
2024-06-14 14:18:11 +0800

    feat(playground): copy color item

--- playground/src/components/ThemeColors.vue
+++ playground/src/components/ThemeColors.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import type { theme } from 'magic-color'
-import { getReadableTextColor } from 'magic-color'
 import { defineProps } from 'vue'
+import ThemeColorItem from './ThemeColorItem.vue'
 
 defineProps<{
   colors: ReturnType<typeof theme>
 }>()
-
-const { copy } = useClipboard()
 </script>
 
 <template>
@@ -16,29 +14,9 @@ const { copy } = useClipboard()
       <li
         v-for="(v, k) in colors"
         :key="k"
-        important-duration-100
-        hover-scale-105 trans fccc
-        p-4 rd-md h-14 md:h-24 text-sm
-        font-mono cursor-pointer :style="{ backgroundColor: v, color: getReadableTextColor({ bgColor: v, textColor: colors[100], fallbackTextColor: colors[900] }) }"
-        @click="copy(v)"
       >
-        <span>{{ k }}</span>
-        <span>{{ v }}</span>
+        <ThemeColorItem :k="k" :v="v" :colors="colors" />
       </li>
     </ul>
   </div>
 </template>
-
-<style scoped>
-table {
-  table-layout: fixed;
-}
-
-th > div {
-  --uno: fcc w-12 h-12 text-size-sm of-hidden rd-md;
-}
-
-td > div {
-  --uno: fcc w-12 h-12 text-size-sm rd;
-}
-</style>
