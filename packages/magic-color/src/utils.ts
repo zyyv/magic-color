47f7013714531b877fcf83bbd7853db45432eb75
Chris <hizyyv@gmail.com>
2024-07-16 16:23:55 +0800

    docs: random desciption

--- packages/magic-color/src/utils.ts
+++ packages/magic-color/src/utils.ts
@@ -1,6 +1,12 @@
 import type { ColorType } from '@magic-color/core'
 import { mc } from './core'
 
+/**
+ * Generate a random color
+ *
+ * @param type ColorType, default is 'hex'
+ * @returns
+ */
 export function random(type: ColorType = 'hex') {
   const color = `#${Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0')}`
   return mc(color, 'hex').css(type)
