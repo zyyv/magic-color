a791602658dd12ab6dc22ab7fa7d57789d7cb7ff
Chris <hizyyv@gmail.com>
2024-10-08 11:08:52 +0800

    chore: type fix

--- talk/composables/useDark.ts
+++ talk/composables/useDark.ts
@@ -19,8 +19,7 @@ export function toggleDark(event: MouseEvent) {
     Math.max(x, innerWidth - x),
     Math.max(y, innerHeight - y),
   )
-  // @ts-expect-error: Transition API
-  const transition = document.startViewTransition(async () => {
+  const transition = (document as any).startViewTransition(async () => {
     isDark.value = !isDark.value
     await nextTick()
   })
