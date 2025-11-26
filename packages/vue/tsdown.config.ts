c52b404f3f3c16cb9a9554c5e0091bf463820a21
Chris <hizyyv@gmail.com>
2025-11-26 10:50:46 +0800

    chore: update

--- packages/vue/tsdown.config.ts
+++ packages/vue/tsdown.config.ts
@@ -3,7 +3,7 @@ import Vue from 'unplugin-vue/rolldown'
 
 export default defineConfig({
   entry: [
-    'src/index.ts',
+    './src/index.ts',
   ],
   platform: 'neutral',
   plugins: [Vue({ isProduction: true })],
